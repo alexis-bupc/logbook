f6f65b1649462754112fb92c56ae3a4936156ca9
alexis-bupc <91877959+alexis-bupc@users.noreply.github.com>
2022-01-24 14:54:58 +0800

    Add files via upload

---
+++ Form1.Designer.vb
@@ -0,0 +1,310 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.PictureBox5 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox4 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox3 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.exitbtn = New System.Windows.Forms.Button()
+        Me.savebtn = New System.Windows.Forms.Button()
+        Me.sexbtngrp = New System.Windows.Forms.GroupBox()
+        Me.femalebtn = New System.Windows.Forms.RadioButton()
+        Me.malebtn = New System.Windows.Forms.RadioButton()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.agetxt = New System.Windows.Forms.TextBox()
+        Me.purposetxt = New System.Windows.Forms.TextBox()
+        Me.contacttxt = New System.Windows.Forms.TextBox()
+        Me.addresstxt = New System.Windows.Forms.TextBox()
+        Me.nametxt = New System.Windows.Forms.TextBox()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.GroupBox1.SuspendLayout()
+        CType(Me.PictureBox5, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox4, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.sexbtngrp.SuspendLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.PictureBox5)
+        Me.GroupBox1.Controls.Add(Me.PictureBox4)
+        Me.GroupBox1.Controls.Add(Me.PictureBox3)
+        Me.GroupBox1.Controls.Add(Me.PictureBox2)
+        Me.GroupBox1.Controls.Add(Me.exitbtn)
+        Me.GroupBox1.Controls.Add(Me.savebtn)
+        Me.GroupBox1.Controls.Add(Me.sexbtngrp)
+        Me.GroupBox1.Controls.Add(Me.Label5)
+        Me.GroupBox1.Controls.Add(Me.Label4)
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Controls.Add(Me.agetxt)
+        Me.GroupBox1.Controls.Add(Me.purposetxt)
+        Me.GroupBox1.Controls.Add(Me.contacttxt)
+        Me.GroupBox1.Controls.Add(Me.addresstxt)
+        Me.GroupBox1.Controls.Add(Me.nametxt)
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 12)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(545, 426)
+        Me.GroupBox1.TabIndex = 0
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Fill-up Form"
+        '
+        'PictureBox5
+        '
+        Me.PictureBox5.ImageLocation = "https://caraga.doh.gov.ph/images/article_images/2021/bida_bakunation.png"
+        Me.PictureBox5.Location = New System.Drawing.Point(432, 278)
+        Me.PictureBox5.Name = "PictureBox5"
+        Me.PictureBox5.Size = New System.Drawing.Size(90, 56)
+        Me.PictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox5.TabIndex = 16
+        Me.PictureBox5.TabStop = False
+        '
+        'PictureBox4
+        '
+        Me.PictureBox4.ImageLocation = "http://assets.rappler.com/612F469A6EA84F6BAE882D2B94A4B421/img/36CD3316067743E99A" &
+    "DC76048ACB7D03/doh-numbers-july-6-2020.jpg"
+        Me.PictureBox4.Location = New System.Drawing.Point(323, 278)
+        Me.PictureBox4.Name = "PictureBox4"
+        Me.PictureBox4.Size = New System.Drawing.Size(90, 56)
+        Me.PictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox4.TabIndex = 15
+        Me.PictureBox4.TabStop = False
+        '
+        'PictureBox3
+        '
+        Me.PictureBox3.ImageLocation = "https://yt3.ggpht.com/ytc/AKedOLTp638LvI-W3tqMvew-qiNZ6AFuxf55-vagDTkn=s900-c-k-c" &
+    "0x00ffffff-no-rj"
+        Me.PictureBox3.Location = New System.Drawing.Point(323, 349)
+        Me.PictureBox3.Name = "PictureBox3"
+        Me.PictureBox3.Size = New System.Drawing.Size(90, 56)
+        Me.PictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox3.TabIndex = 14
+        Me.PictureBox3.TabStop = False
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.ImageLocation = "https://rehansaeed.com/images/hero/Microsoft-.NET-1366x768.png"
+        Me.PictureBox2.Location = New System.Drawing.Point(432, 349)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(90, 56)
+        Me.PictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox2.TabIndex = 13
+        Me.PictureBox2.TabStop = False
+        '
+        'exitbtn
+        '
+        Me.exitbtn.Location = New System.Drawing.Point(147, 349)
+        Me.exitbtn.Name = "exitbtn"
+        Me.exitbtn.Size = New System.Drawing.Size(102, 34)
+        Me.exitbtn.TabIndex = 12
+        Me.exitbtn.Text = "Exit"
+        Me.exitbtn.UseVisualStyleBackColor = True
+        '
+        'savebtn
+        '
+        Me.savebtn.Location = New System.Drawing.Point(26, 349)
+        Me.savebtn.Name = "savebtn"
+        Me.savebtn.Size = New System.Drawing.Size(102, 34)
+        Me.savebtn.TabIndex = 11
+        Me.savebtn.Text = "Save"
+        Me.savebtn.UseVisualStyleBackColor = True
+        '
+        'sexbtngrp
+        '
+        Me.sexbtngrp.Controls.Add(Me.femalebtn)
+        Me.sexbtngrp.Controls.Add(Me.malebtn)
+        Me.sexbtngrp.Location = New System.Drawing.Point(289, 119)
+        Me.sexbtngrp.Name = "sexbtngrp"
+        Me.sexbtngrp.Size = New System.Drawing.Size(233, 117)
+        Me.sexbtngrp.TabIndex = 10
+        Me.sexbtngrp.TabStop = False
+        Me.sexbtngrp.Text = "Sex"
+        '
+        'femalebtn
+        '
+        Me.femalebtn.AutoSize = True
+        Me.femalebtn.Location = New System.Drawing.Point(128, 51)
+        Me.femalebtn.Name = "femalebtn"
+        Me.femalebtn.Size = New System.Drawing.Size(63, 19)
+        Me.femalebtn.TabIndex = 1
+        Me.femalebtn.TabStop = True
+        Me.femalebtn.Text = "Female"
+        Me.femalebtn.UseVisualStyleBackColor = True
+        '
+        'malebtn
+        '
+        Me.malebtn.AutoSize = True
+        Me.malebtn.Location = New System.Drawing.Point(54, 51)
+        Me.malebtn.Name = "malebtn"
+        Me.malebtn.Size = New System.Drawing.Size(51, 19)
+        Me.malebtn.TabIndex = 0
+        Me.malebtn.TabStop = True
+        Me.malebtn.Text = "Male"
+        Me.malebtn.UseVisualStyleBackColor = True
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(289, 33)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(28, 15)
+        Me.Label5.TabIndex = 9
+        Me.Label5.Text = "Age"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(26, 243)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(50, 15)
+        Me.Label4.TabIndex = 8
+        Me.Label4.Text = "Purpose"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(26, 172)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(49, 15)
+        Me.Label3.TabIndex = 7
+        Me.Label3.Text = "Contact"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(26, 101)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(49, 15)
+        Me.Label2.TabIndex = 6
+        Me.Label2.Text = "Address"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(26, 33)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(39, 15)
+        Me.Label1.TabIndex = 5
+        Me.Label1.Text = "Name"
+        '
+        'agetxt
+        '
+        Me.agetxt.Location = New System.Drawing.Point(289, 51)
+        Me.agetxt.Name = "agetxt"
+        Me.agetxt.Size = New System.Drawing.Size(58, 23)
+        Me.agetxt.TabIndex = 4
+        '
+        'purposetxt
+        '
+        Me.purposetxt.Location = New System.Drawing.Point(26, 261)
+        Me.purposetxt.Name = "purposetxt"
+        Me.purposetxt.Size = New System.Drawing.Size(223, 23)
+        Me.purposetxt.TabIndex = 3
+        '
+        'contacttxt
+        '
+        Me.contacttxt.Location = New System.Drawing.Point(26, 190)
+        Me.contacttxt.Name = "contacttxt"
+        Me.contacttxt.Size = New System.Drawing.Size(223, 23)
+        Me.contacttxt.TabIndex = 2
+        '
+        'addresstxt
+        '
+        Me.addresstxt.Location = New System.Drawing.Point(26, 119)
+        Me.addresstxt.Name = "addresstxt"
+        Me.addresstxt.Size = New System.Drawing.Size(223, 23)
+        Me.addresstxt.TabIndex = 1
+        '
+        'nametxt
+        '
+        Me.nametxt.Location = New System.Drawing.Point(26, 51)
+        Me.nametxt.Name = "nametxt"
+        Me.nametxt.Size = New System.Drawing.Size(223, 23)
+        Me.nametxt.TabIndex = 0
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.ImageLocation = "https://doh.gov.ph/sites/default/files/COVID-Case-Bulletin-677-January-20-2022.jp" &
+    "g"
+        Me.PictureBox1.Location = New System.Drawing.Point(563, 12)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(225, 426)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 1
+        Me.PictureBox1.TabStop = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Form1"
+        Me.Text = "DOH - System Logbook"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        CType(Me.PictureBox5, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox4, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.sexbtngrp.ResumeLayout(False)
+        Me.sexbtngrp.PerformLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents sexbtngrp As GroupBox
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents agetxt As TextBox
+    Friend WithEvents purposetxt As TextBox
+    Friend WithEvents contacttxt As TextBox
+    Friend WithEvents addresstxt As TextBox
+    Friend WithEvents nametxt As TextBox
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents PictureBox5 As PictureBox
+    Friend WithEvents PictureBox4 As PictureBox
+    Friend WithEvents PictureBox3 As PictureBox
+    Friend WithEvents PictureBox2 As PictureBox
+    Friend WithEvents exitbtn As Button
+    Friend WithEvents savebtn As Button
+    Friend WithEvents femalebtn As RadioButton
+    Friend WithEvents malebtn As RadioButton
+End Class
---
+++ Form1.vb
@@ -0,0 +1,11 @@
+﻿Public Class Form1
+    Private Sub savebtn_Click(sender As Object, e As EventArgs) Handles savebtn.Click
+        If malebtn.Checked Then
+            MsgBox(nametxt.Text + " " + agetxt.Text + " " + malebtn.Text + " " + addresstxt.Text + " " + contacttxt.Text + " " + purposetxt.Text)
+        ElseIf femalebtn.Checked Then
+            MsgBox(nametxt.Text + " " + agetxt.Text + " " + femalebtn.Text + " " + addresstxt.Text + " " + contacttxt.Text + " " + purposetxt.Text)
+        Else
+            MsgBox("Error 404::!Page not found!")
+        End If
+    End Sub
+End Class
